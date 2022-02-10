2d541c71cdf4ea5656d5237d348132040bf147ae
Pedro Vianna <pedroviannabiomed@gmail.com>
2022-02-09 21:25:27 -0300

    versao 0.0.1 inserindo no github projeto final

---
+++ class/anot.md
@@ -0,0 +1,31 @@
+https://github.com/Pedro-Arantes/aula6-POO-exercicio
+
+
+
+exercicio aula 7 :
+
+
+tblProdutos
+
+idproduto Pk AI
+produto
+Preco float(10,2)
+qtdEstoque int
+fornecedor varchar (50)
+
+
+https://github.com/Pedro-Arantes/aula7-POO-Exercicio
+
+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
+
+
+Pesquisar:
+
+-Angular
+-React
+
+-No SQL
+-MongoDB(Python)
+-Firebase(Python)
+    *banco de dados do GOOGLE.
+    
---
+++ navbar.php
@@ -0,0 +1,39 @@
+<div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex justify-content-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope"></i> <a href="mailto:developer9pedro@gmail.com">developer9pedro@gmail.com</a>
+        <i class="bi bi-phone"></i> +55 2189 55488 55
+      </div>
+      <div class="d-none d-lg-flex social-links align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><img src="ico_clinica.webp" alt="" class="img-fluid"><a href="index.html">CliniMax</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!--<a href="index.html" class="logo me-auto"><img src="../template/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.php">Pagina Inicial</a></li>
+          <li><a class="nav-link scrollto" href="#about">Sobre</a></li>
+          <li><a class="nav-link scrollto" href="#services">Serviços</a></li>
+          <li><a class="nav-link scrollto" href="#departments">Departamentos</a></li>
+          <li><a class="nav-link scrollto" href="#doctors">Doutores</a></li>
+          
+          
+          <li><a class="nav-link scrollto" href="#contact">Contato</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
